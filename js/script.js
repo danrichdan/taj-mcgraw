e5f49a8e95f2bf8bfd40fe7ddedd5d05447ec283
Dan Riches <driches@ymail.com>
2017-10-18 11:17:12 -0700

    compressed the main image, designed and styled for multiple devices, light box completed and modal completed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
 
 function applyClickHandlers() {
     mobileNavigation();
-}
+    lightBox();
+    animateToTopButton();
+    backToTop();
+};
 
 //FUNCTION FOR THE HAMBURGER MENU
 function mobileNavigation() {
@@ -12,10 +15,67 @@ function mobileNavigation() {
     $hamburgerMenu.click(function(){
         var $hamburgerContainer = this;
         $("div.hamburger-menu-container").find("div").toggleClass("change");
-        console.log("Clicking on hamburger");
         $(".main-nav").toggleClass("hide-show responsive");
     });
 };
 
+//FUNCTION FOR IMAGE GALLERY LIGHTBOX
+function lightBox() {
+    //Create overlay
+    var $overlay = $('<div>', {
+        id: "overlay",
+    });
 
+    var $image = $("<img>");
+
+    //Add An image to overlay
+    $overlay.append($image);
+
+    //Add overlay to page
+    $("body").append($overlay);
+
+    //Capture the click event on a link to an image
+    $("#imageGallery a").click(function(event){
+        event.preventDefault();
+        var imageLocation = $(this).attr("href");
+
+        //Update overlay with the image linked in the link
+        $image.attr("src", imageLocation);
+
+        //Show the overlay.
+        $overlay.show();
+    });
+    //When overlay is clicked
+    $overlay.click(function(){
+        //Hide the overlay
+        $($overlay).hide();
+    });
+};
+
+
+//DISPLAY BACK TO TOP BUTTON
+function animateToTopButton() {
+    var $window = $(window);
+    var $toTopButton = $('#back-to-top');
+    $toTopButton.hide();
+    var endZone = $('footer').offset().top - $window.height() - 3000;
+    $window.on('scroll', function(){
+        if( (endZone) < $window.scrollTop() ) {
+            $toTopButton.show();
+        } else {
+            $toTopButton.hide();
+        }
+    });
+};
+
+//BACK TO TOP CLICK HANDLER
+function backToTop() {
+    $('#back-to-top').click(function( event ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("header").offset().top
+        }, 1200);
+        $('#back-to-top').hide();
+    });
+}
 
